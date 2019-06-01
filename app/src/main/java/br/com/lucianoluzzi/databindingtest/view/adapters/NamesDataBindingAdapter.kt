8b14e2f6876863da30a48b25403a0bc24e451519
Luciano <lucianoluzzi@hotmail.com>
2019-06-01 20:26:32 -0300

    refactoring

--- app/src/main/java/br/com/lucianoluzzi/databindingtest/view/adapters/NamesDataBindingAdapter.kt
+++ app/src/main/java/br/com/lucianoluzzi/databindingtest/view/adapters/NamesDataBindingAdapter.kt
@@ -1,12 +1,14 @@
 package br.com.lucianoluzzi.databindingtest.view.adapters
 
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.lucianoluzzi.databindingtest.model.NameModel
 import br.com.lucianoluzzi.databindingtest.model.NamesAdapter
 
 @BindingAdapter("names", requireAll = false)
 fun RecyclerView.setNames(names: List<NameModel>) {
-    val adapter = NamesAdapter(this.context, names)
+    val adapter = NamesAdapter(context, names)
     this.adapter = adapter
+    layoutManager = LinearLayoutManager(context)
 }
