981b2ef28fef59c059285246a04ea6feb06d27a5
Luciano <lucianoluzzi@hotmail.com>
2019-06-01 23:46:40 -0300

    refactoring

--- app/src/main/java/br/com/lucianoluzzi/databindingtest/view/MainActivity.kt
+++ app/src/main/java/br/com/lucianoluzzi/databindingtest/view/MainActivity.kt
@@ -3,7 +3,6 @@ package br.com.lucianoluzzi.databindingtest.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.lucianoluzzi.databindingtest.R
 import br.com.lucianoluzzi.databindingtest.databinding.ActivityMainBinding
 import br.com.lucianoluzzi.databindingtest.model.NameViewModel
@@ -16,11 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-    }
-
-    override fun onResume() {
-        super.onResume()
         binding.viewModel = nameViewModel
-        binding.recycler.layoutManager = LinearLayoutManager(this)
     }
 }
